949528a34567ea3af8b2e023fdf21cb18660ad3e
Daniel Kulichkov <danchick03@gmail.com>
2024-10-30 11:44:45 +0300

    feat: lchange git profile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::path::PathBuf;
+use std::process::Command;
+
+use dirs::home_dir;
+
+mod config_model;
+use config_model::{Config, Profile};
+
 fn main() {
-    println!("Hello, world!");
+    let input_profile = "home";
+
+    let config_path = get_config_path();
+    let config = Config::from_file(config_path).unwrap();
+
+    let profile = config
+        .profiles
+        .get(input_profile)
+        .expect("failed to find profile");
+
+    change_profile(profile);
+
+    println!("Changed git profile: {}", &profile.email);
+}
+
+fn get_config_path() -> PathBuf {
+    let home = home_dir().expect("failed to get your home directory");
+
+    home.join(".config").join("gprof").join("profiles")
+}
+
+fn change_profile(profile: &Profile) {
+    Command::new("git")
+        .arg("config")
+        .arg("--global")
+        .arg("user.email")
+        .arg(&profile.email)
+        .status()
+        .unwrap();
+
+    Command::new("git")
+        .arg("config")
+        .arg("--global")
+        .arg("user.name")
+        .arg(&profile.name)
+        .status()
+        .unwrap();
 }
