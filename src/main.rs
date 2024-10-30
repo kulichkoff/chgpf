290feaf9e2b5664b564735ec2c2e4054425713d8
Daniel Kulichkov <danchick03@gmail.com>
2024-10-31 00:28:49 +0300

    feat: initializa logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use std::env;
+use std::process::exit;
+
 use chgpf::config_model::Config;
 use chgpf::{change_profile, get_config_path, get_profile_argument};
 
 fn main() {
+    initializa_logger();
+    log::info!("Application started");
+
     let input_profile = get_profile_argument();
 
     let config_path = get_config_path();
-    let config = Config::from_file(config_path).unwrap();
+    let config = match Config::from_file(config_path) {
+        Ok(conf) => conf,
+        Err(err) => {
+            log::error!("Failed to load config: {}", err);
+            exit(1);
+        }
+    };
 
     let profile = config
         .profiles
@@ -15,4 +27,18 @@ fn main() {
     change_profile(profile);
 
     println!("Changed git profile: {}", &profile.email);
+}
+
+fn initializa_logger() {
+    let log_level = env::var("RUST_LOG").unwrap_or_default();
+    
+    if log_level.is_empty() {
+        env_logger::Builder::new()
+            .filter(None, log::LevelFilter::Off)
+            .init();
+    } else {
+        env_logger::Builder::new()
+            .filter(None, log::LevelFilter::Info)
+            .init();
+    }
 }
