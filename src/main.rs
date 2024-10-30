10b6415dc2f03d60394debb191567a724cba910e
Daniel Kulichkov <danchick03@gmail.com>
2024-10-31 01:03:44 +0300

    feat: simplify initialization

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use chgpf::config_model::Config;
 use chgpf::{change_profile, get_config_path, get_profile_argument};
 
 fn main() {
-    initializa_logger();
+    env_logger::init();
     log::info!("Application started");
 
     let input_profile = get_profile_argument();
@@ -15,7 +15,6 @@ fn main() {
         Ok(conf) => conf,
         Err(err) => {
             log::error!("Failed to load config: {}", err);
-            eprintln!("Failed to find load config. Ensure you have created it");
             exit(1);
         }
     };
@@ -28,18 +27,4 @@ fn main() {
     change_profile(profile);
 
     println!("Changed git profile: {}", &profile.email);
-}
-
-fn initializa_logger() {
-    let log_level = env::var("RUST_LOG").unwrap_or_default();
-    
-    if log_level.is_empty() {
-        env_logger::Builder::new()
-            .filter(None, log::LevelFilter::Off)
-            .init();
-    } else {
-        env_logger::Builder::new()
-            .filter(None, log::LevelFilter::Info)
-            .init();
-    }
 }
