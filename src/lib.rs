68ce4ae8819a2b4a23a54a1d95b4fd402f4bee45
Daniel Kulichkov <danchick03@gmail.com>
2024-10-31 01:12:46 +0300

    feat: add messages in core logic

--- src/lib.rs
+++ src/lib.rs
@@ -7,35 +7,47 @@ use std::process::{exit, Command};
 use config_model::Profile;
 
 pub fn change_profile(profile: &Profile) {
-    Command::new("git")
+    if let Err(err) = Command::new("git")
         .arg("config")
         .arg("--global")
         .arg("user.email")
         .arg(&profile.email)
         .status()
-        .unwrap();
+    {
+        log::error!("Failed to execute git command: {}", err);
+        exit(1);
+    }
 
-    Command::new("git")
+    if let Err(err) = Command::new("git")
         .arg("config")
         .arg("--global")
         .arg("user.name")
         .arg(&profile.name)
         .status()
-        .unwrap();
+    {
+        log::error!("Failed to execute git command: {}", err);
+        exit(1);
+    }
 }
 
 pub fn get_config_path() -> PathBuf {
-    let home = dirs::home_dir().expect("failed to get your home directory");
+    let home = match dirs::home_dir() {
+        Some(path) => path,
+        None => {
+            log::error!("Failed to find home directory");
+            exit(1);
+        }
+    };
     home.join(".config").join("chgpf").join("profiles")
 }
 
 pub fn get_profile_argument() -> String {
     let args: Vec<String> = env::args().collect();
-    
+
     if args.len() != 2 {
         println!("Usage: chgpf [profile_name]");
         exit(1);
     }
-    
+
     args[1].clone()
 }
