5ac52b3e29d5eb34cff94cf875bc14c20b8675eb
Daniel Kulichkov <danchick03@gmail.com>
2026-05-10 09:52:10 +0300

    ref: parse CLI with bpaf crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,3 @@
 pub mod app;
 pub mod config;
 pub mod git;
-
-use std::env;
-use std::process::exit;
-
-pub fn get_profile_argument() -> String {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 2 {
-        println!("Usage: chgpf [profile_name]");
-        exit(1);
-    }
-
-    args[1].clone()
-}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,54 @@
+use bpaf::*;
 use std::process::exit;
 
 use chgpf::config::Profiles;
-use chgpf::get_profile_argument;
 use chgpf::git;
 
+#[derive(Debug, Clone)]
+enum Command {
+    Switch { profile: String },
+    List,
+}
+
+fn switch_cmd() -> impl Parser<Command> {
+    positional::<String>("PROFILE").map(|profile| Command::Switch { profile })
+}
+
+fn list_cmd() -> impl Parser<Command> {
+    pure(Command::List).to_options().command("list")
+}
+
+fn parser() -> OptionParser<Command> {
+    construct!([list_cmd(), switch_cmd(),])
+        .to_options()
+        .descr("Switch Git profiles")
+}
+
 fn main() {
-    let input_profile = get_profile_argument();
+    let cmd = parser().run();
 
-    let config = match Profiles::from_configured() {
-        Ok(conf) => conf,
-        Err(_) => {
-            exit(1);
+    match cmd {
+        Command::List => todo!(),
+        Command::Switch { profile } => {
+            let config = match Profiles::from_configured() {
+                Ok(conf) => conf,
+                Err(_) => {
+                    exit(1);
+                }
+            };
+
+            let profile = match config.get(&profile) {
+                Some(prof) => prof,
+                None => {
+                    exit(1);
+                }
+            };
+
+            if git::Config::set_profile(profile).is_err() {
+                exit(1)
+            }
+
+            println!("Switched: {}", &profile.email);
         }
     };
-
-    let profile = match config.get(&input_profile) {
-        Some(prof) => prof,
-        None => {
-            exit(1);
-        }
-    };
-
-    if git::Config::set_profile(profile).is_err() {
-        exit(1)
-    }
-
-    println!("Changed git profile: {}", &profile.email);
 }
