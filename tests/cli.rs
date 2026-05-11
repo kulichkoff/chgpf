297dd637c13f8518c3e7791e39957238c17442e2
Daniel Kulichkov <danchick03@gmail.com>
2026-05-11 14:39:38 +0300

    test: init command execution

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,13 @@
+use std::{fs, path::PathBuf};
+
 use serial_test::serial;
+
+use crate::common::TestEnv;
 mod common;
+
+fn profiles_path(test_env: &TestEnv) -> PathBuf {
+    test_env.xdg.join("chgpf").join("profiles")
+}
 
 #[test]
 #[serial]
@@ -37,7 +45,7 @@ fn list_without_config_exits_nonzero() {
 
     let expected_err = format!(
         "Error: config file not found: {}\n",
-        test_env.xdg.join("chgpf").join("profiles").to_str().unwrap()
+        profiles_path(&test_env).to_str().unwrap()
     );
 
     fixture.assert().failure().stderr(expected_err);
@@ -69,4 +77,40 @@ fn list_with_seeded_profiles_prints_stable_output() {
     let out_str = String::from_utf8(output.stdout).unwrap();
 
     insta::assert_snapshot!(out_str);
+}
+
+#[test]
+#[serial]
+fn init_with_clean_state() {
+    let test_env = common::TestEnv::new();
+    let mut fixture = test_env.cmd();
+    fixture.arg("init");
+
+    fixture.assert().success();
+
+    let profiles_path = profiles_path(&test_env);
+    let profiles = fs::read_to_string(profiles_path).unwrap();
+    insta::assert_snapshot!(profiles);
+}
+
+#[test]
+#[serial]
+fn init_twice_or_force() {
+    let test_env = common::TestEnv::new();
+
+    // 1st run of "init"
+    let mut fixture = test_env.cmd();
+    fixture.arg("init");
+    fixture.assert().success();
+
+    // 2nd "init": failed
+    let mut fixture = test_env.cmd();
+    fixture.arg("init");
+    fixture.assert().failure();
+
+    // additional "init" with "--force" flag
+    let mut fixture = test_env.cmd();
+    fixture.args(["init", "--force"]);
+
+    fixture.assert().success();
 }
